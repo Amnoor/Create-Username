23794c04caa4a4e9f9546ca50c3f032180520b32
Amnoor Brar <TechMan1810@gmail.com>
2025-10-04 20:13:34 +0800

    I want you to make me a formal about the changes I made in the @diff:Create-Username. It should be detailed but short and simple to understand but don't oversimplify it.

--- create username.py
+++ create username.py
@@ -1,14 +1,33 @@
-#   asking the user to create ther username, but there username can't contain spaces, digits, and should not exceed more than 12 characters.
-#   asking the user input to create a username.
+# create username
+# this is a project I made to practice my skills, it let's you create username with a set of rules that you cant break
+# rules:
+# username can't contain spaces, digits, and should not exceed more than 12 characters
+
+# ask user for username
 username = input("Enter your username (your username can't contain spaces, digits, and should not exceed more than 12 characters): ")
-#   checking if the username does not contain spaces, digits, and should not exceed more than 12 characters.
+# check if username is valid
 check_spaces_and_digits = username.isalpha()
 check_character = len(username)
-while check_spaces_and_digits == False or check_character > 12 or username == "":
-#   if it does contain spaces, digits, and exceed more than 12 characters, it will print out "Invalid username! Please Try again later."
+# if username is valid, then I wille set the variable is_valid to True
+if check_spaces_and_digits == False and check_character >= 12 and username == "":
+    is_valid = True
+# else, I will set the variable is_valid to False
+else:
+    is_valid = False
+# while the variable is_valid is True
+while is_valid:
+#   I will print a Error Message and ask the user to try again
     print("Invalid username! Please Try again later.")
+#   ask user for username again
     username = input("Enter your username (your username can't contain spaces, digits, and should not exceed more than 12 characters): ")
+#   check if username is valid again
     check_spaces_and_digits = username.isalpha()
     check_character = len(username)
-#   if it does not contain spaces, digits, and should not exceed more than 12 characters, it will print out "You have successfully created your username!😊"
+#   if username is valid, then I wille set the variable is_valid to True and repeat this while loop
+    if check_spaces_and_digits == False and check_character >= 12 and username == "":
+        is_valid = True
+#   else, I will set the variable is_valid to Fals, so the while loop will end
+    else:
+        is_valid = False
+# print a success message
 print("You have successfully created your username!😊")
